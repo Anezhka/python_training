286a09a5231a4b2ef68b698238258578c2ef4edd
Anezhka <40922520+Anezhka@users.noreply.github.com>
2025-07-09 23:59:37 +0200

    Fixture, tests, application.

---
+++ test_add_group.py
@@ -0,0 +1,22 @@
+from application import Application
+from group import Group
+import pytest
+
+@pytest.fixture
+def app(request):
+    fixture = Application()
+    request.addfinalizer(fixture.destroy)
+    return fixture
+
+def test_add_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("PythonTestName", "testHeader", "testFooter"))
+    app.logout()
+
+def test_add_empty_group(app):
+    app.login("admin", "secret")
+    app.create_group(Group("", "", ""))
+    app.logout()
+
+
+# 9 next

f25a5fa6bc9cf5a93a1f9e40a433064a86de4ab2
Anezhka <40922520+Anezhka@users.noreply.github.com>
2025-07-11 01:02:56 +0200

    Refactoring.

---
+++ test/test_del_group.py
@@ -0,0 +1,6 @@
+from model.group import Group
+
+def test_delete_first_group(app):
+    if app.group.count() == 0:
+        app.group.create(Group(name="TestName", header="TestHeader", footer="TestFooter"))
+    app.group.delete_first_group()

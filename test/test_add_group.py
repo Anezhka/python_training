859fbf6b2d8edaffd333fab4eeccaff310aa84ad
Anezhka <40922520+Anezhka@users.noreply.github.com>
2025-07-13 19:32:29 +0200

    Cash and Hash, index.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -2,14 +2,22 @@ from model.group import Group
 
 def test_add_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("PythonTestName", "testHeader", "testFooter"))
+    group = Group("PythonTestName", "testHeader", "testFooter")
+    app.group.create(group)
+    assert len(old_groups) + 1 == app.group.count() # app.group.count - хэш для предварительной проверки
     new_groups = app.group.get_group_list()
-    assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 def test_add_empty_group(app):
     old_groups = app.group.get_group_list()
-    app.group.create(Group("", "", ""))
+    group = Group("", "", "")
+    app.group.create(group)
     new_groups = app.group.get_group_list()
     assert len(old_groups) + 1 == len(new_groups)
+    old_groups.append(group)
+    assert sorted(old_groups, key=Group.id_or_max) == sorted(new_groups, key=Group.id_or_max)
+
 
 
